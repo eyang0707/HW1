2fb4e4d27740cd59193a95aacd522e2281f50281
eyang0707 <eyang@cca.edu>
2015-09-07 00:32:01 -0700

    Create puzzle5.js

---
+++ puzzle5.js
@@ -0,0 +1,30 @@
+right();
+right();
+
+if (getColor() == "red") {
+  up();
+} else {
+  left();
+}
+
+right();
+right();
+
+if (getColor() == "red") {
+  up();
+} else {
+  left();
+}
+
+right();
+right();
+
+if (getColor() == "blue") {
+  down();
+} else {
+  left();
+}
+
+right();
+right();
+right();
